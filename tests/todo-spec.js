e3d04b1b3fe4c3ed44aa7acab3b06d0f2fd19dce
Ilya Kuydich <agree2012@yandex.ru>
2017-06-06 15:00:25 +0400

    Update todo-spec.js

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -1,46 +1,17 @@
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', function() {
+    browser.get('https://angularjs.org');
 
-  describe('todo list', function() {
-    browser.get('http://www.angularjs.org');
-    it('should greet the named user', function() {
-    
-      
-    
-      element(by.model('yourName')).sendKeys('Julie');
+    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    element(by.css('[value="add"]')).click();
 
-      var greeting = element(by.binding('yourName'));
+    var todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write first protractor test');
 
-      expect(greeting.getText()).toEqual('Hello Julie!');
-     });
-    
-   var todoList;
-
-   beforeEach(function() {
-     
-      todoList = element.all(by.repeater('todo in todoList.todos'));
-    });
-
-    it('should list todos', function() {
-      expect(todoList.count()).toEqual(2);
-      expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-    });
-
-    it('should add a todo', function() {
-      var addTodo = element(by.model('todoList.todoText'));
-      var addButton = element(by.css('[value="add"]'));
-
-      addTodo.sendKeys('write a protractor test');
-      addButton.click();
-
-      expect(todoList.count()).toEqual(3);
-      expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
+    // You wrote your first test, cross it off the list
+    todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
   });
-
+});
