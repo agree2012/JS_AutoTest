242d23eb3b7db17ea57e1315ed726fa6041039d7
Ilya Kuydich <agree2012@yandex.ru>
2017-06-06 12:03:23 +0400

    Update todo-spec.js

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -1,16 +1,37 @@
-'use strict';
+describe('angularjs homepage', function() {
+  it('should greet the named user', function() {
+    browser.get('http://www.angularjs.org');
 
-var AngularPage = require('./pageObject/angular_page.js');
-global.angularPage = new AngularPage();
-var pageObject;
+    element(by.model('yourName')).sendKeys('Julie');
 
+    var greeting = element(by.binding('yourName'));
 
-describe('angularJs homepage ', function () {
-    describe('Greetings section', function () {
-        it('should greet the user', function () {
-            pageObject = angularPage.getAngularPage();
-            pageObject.typeName('Bob');
-            expect(pageObject.greeting).toEqual('Hello Bob!')
-         });
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  describe('todo list', function() {
+    var todoList;
+
+    beforeEach(function() {
+      browser.get('http://www.angularjs.org');
+
+      todoList = element.all(by.repeater('todo in todoList.todos'));
     });
+
+    it('should list todos', function() {
+      expect(todoList.count()).toEqual(2);
+      expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
+    });
+
+    it('should add a todo', function() {
+      var addTodo = element(by.model('todoList.todoText'));
+      var addButton = element(by.css('[value="add"]'));
+
+      addTodo.sendKeys('write a protractor test');
+      addButton.click();
+
+      expect(todoList.count()).toEqual(3);
+      expect(todoList.get(2).getText()).toEqual('write a protractor test');
+    });
+  });
 });
