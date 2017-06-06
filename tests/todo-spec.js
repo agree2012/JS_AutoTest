20352b2142730d4ba01a05ebe1c88e8fb83994ef
Ilya Kuydich <agree2012@yandex.ru>
2017-06-06 14:35:46 +0400

    Update todo-spec.js

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -2,7 +2,7 @@ describe('angularjs homepage', function() {
   it('should greet the named user', function() {
     
     browser.get('http://www.angularjs.org');
-    sleep(20000);
+   
 
     element(by.model('yourName')).sendKeys('Julie');
 
