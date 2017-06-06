8335e374f38a513726489d17510a2e69d2389e6e
Ilya Kuydich <agree2012@yandex.ru>
2017-06-06 14:41:18 +0400

    Update todo-spec.js

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -1,25 +1,6 @@
 describe('todo list', function() {
-    var todoList;
-
-    beforeEach(function() {
-      browser.get('http://www.angularjs.org');
-
-      todoList = element.all(by.repeater('todo in todoList.todos'));
-    });
+    var todoList = 1;
 
     it('should list todos', function() {
-      expect(todoList.count()).toEqual(2);
-      expect(todoList.get(1).getText()).toEqual('build an AngularJS app');
-    });
-
-    it('should add a todo', function() {
-      var addTodo = element(by.model('todoList.todoText'));
-      var addButton = element(by.css('[value="add"]'));
-
-      addTodo.sendKeys('write a protractor test');
-      addButton.click();
-
-      expect(todoList.count()).toEqual(3);
-      expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
+      expect(todoList).toEqual(2);});
   });
